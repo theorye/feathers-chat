69f7e63cc355838ee9d83eedef3309a096483dc3
Kyoji Kobayashi <Kyoji Kobayashi>
2017-04-03 15:28:45 -0700

    Changed from Princess Toadstill into a different castle

--- public/react/src/login.js
+++ public/react/src/login.js
@@ -39,19 +39,24 @@ export default class Login extends Component {
       <div className="row">
         <div className="col-12 col-6-tablet push-3-tablet col-4-desktop push-4-desktop login-container box-shadow border-radius">
           <div className="login-header-card border-radius box-shadow">
+            <div className="login-title">Login</div>
             <div class="social-line">
               <a href="#btn" className="login-icon">
-                <img className="login-icon" src="img/icons/Facebook.svg" />
+                <img className="login-icon" src="http://localhost:3030/img/icons/Facebook.svg" />
               </a>
               <a href="#btn" className="login-icon">
-                <img className="login-icon" src="img/icons/Twitter.svg" />
+                <img className="login-icon" src="http://localhost:3030/img/icons/Twitter.svg" />
               </a>
               <a href="#btn" className="login-icon">
-                <img className="login-icon" src="img/icons/Google+.svg" />
+                <img className="login-icon" src="http://localhost:3030/img/icons/Google+.svg" />
+              </a>
+              <a href="#btn" className="login-icon">
+                <img className="login-icon" src="http://localhost:3030/img/icons/Github.svg" />
               </a>
             </div>
           </div>
           <form className="form">
+            <div className="login-title">- Or create an account with us -</div>
             <fieldset>
               <input className="block border-radius box-shadow-light login-field" type="email" name="email" placeholder="email" onChange={ev => this.updateField('email', ev)} />
             </fieldset>
@@ -59,6 +64,8 @@ export default class Login extends Component {
             <fieldset>
               <input className="block border-radius box-shadow-light login-field" type="password" name="password" placeholder="password" onChange={ev => this.updateField('password', ev)} />
             </fieldset>
+
+            <input type="checkbox" /> <label>Save Password</label>
 
             <button type="button" className="button button-primary block signup box-shadow-light" onClick={() => this.login()}>
               Log in
